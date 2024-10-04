5abf989f6ae50227edb047c188ecbb4195bf9a0d
Nathanlo200 <nathanlomito@gmail.com>
2024-10-04 16:58:42 +0100

    feat : integration de la prise en compte du modes sombre et clair et amelioration de l'interface dans le fichier VotePage2.dart

--- lib/m_evaluation/ui/pages/vote/VotePage2.dart
+++ lib/m_evaluation/ui/pages/vote/VotePage2.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
-import 'package:odc_mobile_project/m_evaluation/ui/composants/AppSize.dart';
+import '../../composants/AppSize.dart';
 import '../../composants/CustomSlider.dart';
 import '../intervenantPage/intervenantCtrl.dart';
 import 'VoteCtrl.dart';
@@ -25,7 +25,6 @@ class _VotePageState extends ConsumerState<VotePage2> {
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) async {
-      // action initiale de la page et appel d'un controleur
       var intervenantId = widget.phaseIntervenantId;
       print("intervenantId $intervenantId");
       var ctrl = ref.read(voteCtrlProvider.notifier);
@@ -48,6 +47,13 @@ class _VotePageState extends ConsumerState<VotePage2> {
   @override
   Widget build(BuildContext context) {
     var state = ref.watch(voteCtrlProvider);
+    AppSize().init(context);
+    int allSlidersMoved = state.criteres.where((critere) => state.valeurs["${critere.critere.id}"] != 0).length;
+
+    bool isDarkTheme = Theme.of(context).brightness == Brightness.dark;
+    Color loadingColor1 = isDarkTheme
+        ? Colors.white
+        : Colors.black;
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -62,17 +68,29 @@ class _VotePageState extends ConsumerState<VotePage2> {
             color: Color(0xffFF7900).withOpacity(0.8),
           ),
         ),
+        actions: [
+          Text(
+            "$allSlidersMoved/${state.criteres.length} Critères ",
+            style: TextStyle(
+                color: loadingColor1,
+                fontSize: 15,
+                height: 2.5,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+        ],
       ),
-      body: SingleChildScrollView(
-        child: SafeArea(
+      body: SafeArea(
             child: Stack(
           children: [
             _contenuPrincipale(context, ref),
             _chargement(context, ref),
-            _SubmitButton2(context, ref),
+             _SubmitButton(context, ref),
           ],
         )),
-      ),
     );
   }
 
@@ -80,45 +98,14 @@ class _VotePageState extends ConsumerState<VotePage2> {
     var state = ref.watch(voteCtrlProvider);
     return Container(
       child: Center(
-        child: Column(
-          children: <Widget>[
+        child: Container(
+        child:
             Align(
               alignment: Alignment.topCenter,
               child: Stack(
                 children: [
-                  Container(
-                    margin: EdgeInsets.fromLTRB(135, 0, 0, 0),
-                    width: 300,
-                    height: 400,
-                    child: Center(
-                      child: Image.asset(
-                        "images/x.png",
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 300, 0, 0),
-                    width: 300,
-                    height: 300,
-                    child: Center(
-                      child: Image.asset(
-                        "images/x.png",
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(135, 600, 0, 0),
-                    width: 300,
-                    height: 200,
-                    child: Center(
-                      child: Image.asset(
-                        "images/x.png",
-                      ),
-                    ),
-                  ),
                   Center(
                     child: Column(
-                      mainAxisSize: MainAxisSize.min,
                       children: [
                         Stack(
                           alignment: Alignment.center,
@@ -127,27 +114,9 @@ class _VotePageState extends ConsumerState<VotePage2> {
                               Icons.person,
                               size: 100,
                             ),
-                            Positioned(
-                              top: 0,
-                              right: 0,
-                              child: Container(
-                                padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
-                                decoration: BoxDecoration(
-                                  color: Colors.red,
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                child: Text(
-                                  '${state.criteres.length.toString()}+criteres',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 15,
-                                  ),
-                                ),
-                              ),
-                            ),
                           ],
                         ),
-                        SizedBox(height: 10), // Space between the icon and the email text
+                        SizedBox(height: 5), // Space between the icon and the email text
                         Text(
                           "${state.phaseVoteIntervenants?.email}",
                           style: TextStyle(fontSize: 17),
@@ -157,7 +126,6 @@ class _VotePageState extends ConsumerState<VotePage2> {
                     ),
                   ),
                   Container(
-                    padding: EdgeInsets.only(bottom: 40),
                     decoration: BoxDecoration(
                       color: Colors.white.withOpacity(0.6),
                       borderRadius: BorderRadius.only(
@@ -171,10 +139,19 @@ class _VotePageState extends ConsumerState<VotePage2> {
                     child: ListView.builder(
                         itemCount: state.criteres.length,
                         itemBuilder: (BuildContext context, int index) {
+                          bool isDarkTheme = Theme.of(context).brightness == Brightness.dark;
+                          Color loadingColor1 = isDarkTheme
+                              ? Colors.white
+                              : Colors.white;
                           var critere = state.criteres[index].critere;
                           print(critere.toJson());
                           var savedValue =
                               state.valeurs["${critere.id}"] ?? 0.0;
+                          var state2 = ref.read(intervenantCtrlProvider);
+                          var intervenant = state2.intervenants.firstWhere(
+                                  (element) => element.intervenantId == widget.phaseIntervenantId,
+                          );
+
                           return Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             crossAxisAlignment: CrossAxisAlignment.center,
@@ -185,16 +162,20 @@ class _VotePageState extends ConsumerState<VotePage2> {
                                 child: Column(
                                   children: [
                                     Card(
+                                      color: loadingColor1,
                                       child: ListTile(
                                         leading: Text(
                                           (index + 1).toString(),
                                           style: TextStyle(
                                             fontSize: 20,
+                                            color: Colors.black,
                                           ),
                                         ),
                                         title: Text(
                                           state.criteres[index].critere.libelle,
-                                          style: TextStyle(fontSize: 20),
+                                          style: TextStyle(fontSize: 20,
+                                          color: Colors.black,
+                                          ),
                                         ),
                                         trailing: Text(
                                           "Ponderation : " +
@@ -203,13 +184,13 @@ class _VotePageState extends ConsumerState<VotePage2> {
                                                   .toString(),
                                           style: TextStyle(
                                             fontSize: 15,
-                                            color: Colors.black54,
+                                            color: Colors.black,
                                           ),
                                         ),
                                       ),
                                     ),
                                     Card(
-                                      margin: EdgeInsets.all(4),
+                                      margin: EdgeInsets.all(0.3),
                                       color: Colors.white70,
                                       shape: RoundedRectangleBorder(
                                         borderRadius:
@@ -249,8 +230,10 @@ class _VotePageState extends ConsumerState<VotePage2> {
                                                 padding: EdgeInsets.fromLTRB(
                                                     10, 20, 10, 10),
                                                 child: CustomSlider(
+                                                  isEnabled: !intervenant.isDone,
                                                   title: '',
                                                   onSliderChanged: (value) {
+
                                                     var ctrl = ref.read(
                                                         voteCtrlProvider
                                                             .notifier);
@@ -312,71 +295,7 @@ class _VotePageState extends ConsumerState<VotePage2> {
                 ],
               ),
             )
-          ],
-        ),
-      ),
-    );
-  }
-
-  _SubmitButton2(BuildContext context, WidgetRef ref) {
-    var state = ref.watch(voteCtrlProvider);
-    return Container(
-      alignment: Alignment.bottomRight,
-      width: AppSize.screenWidth * 0.95,
-      height: AppSize.screenHeight * 0.86,
-      child: FloatingActionButton(
-        backgroundColor: Colors.orange,
-        child: Icon(Icons.send),
-        onPressed: () async {
-          var res = await showDialog<String>(
-              context: context,
-              builder: (BuildContext context) => AlertDialog(
-                    title: const Text(
-                      "Submit Vote !",
-                      style: TextStyle(color: Colors.orange, fontSize: 35),
-                    ),
-                    content: const Text(
-                      "You're to the point of submitting Vote",
-                      style: TextStyle(fontSize: 15),
-                    ),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () => Navigator.pop(context, "Cancel"),
-                        child: const Text(
-                          "Cancel",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ),
-                      TextButton(
-                          onPressed: () async {
-                            Navigator.of(context).pop('Yes');
-                          },
-                          child: const Text(
-                            "Yes",
-                            style: TextStyle(fontSize: 20),
-                          ))
-                    ],
-                  ));
-
-          if (res == "Yes") {
-            setState(() {
-              state.isLoading = true;
-            });
-            await Future.delayed(Duration(seconds: 3));
-            print(
-                "WiDGETINTERN ${widget.phaseIntervenantId}, phaseId ${widget.phaseId}");
-            var ctrl = ref.read(voteCtrlProvider.notifier);
-            await ctrl.sendVoteResulats(
-                widget.phaseIntervenantId, widget.phaseId);
-            if (mounted) {
-              setState(() {
-                state.isLoading = false;
-              });
-              await _chargement(context, ref);
-              context.pop(true);
-            }
-          }
-        },
+    )
       ),
     );
   }
@@ -394,5 +313,74 @@ class _VotePageState extends ConsumerState<VotePage2> {
                 color: Colors.black, size: 50),
           ),
         ));
+  }
+  _SubmitButton(BuildContext context, WidgetRef ref) {
+    var state = ref.watch(voteCtrlProvider);
+    return Center(
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          margin: EdgeInsets.only(bottom: 60),
+                width: 200,
+                child: ElevatedButton( onPressed: () async {
+                  var res =await showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text(
+                          "Soumission de votre vote.",
+                          style: TextStyle(color: Colors.orange, fontSize: 30),
+                        ),
+                        content: const Text(
+                          "Êtes-vous sûr de vouloir soumettre votre vote ?",
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, "Annulez"),
+                            child: const Text(
+                              "Annulez",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                          TextButton(
+                              onPressed: () async {
+                                Navigator.of(context).pop('Oui');
+                              },
+                              child: const Text(
+                                "Oui",
+                                style: TextStyle(fontSize: 20),
+                              ))
+                        ],
+                      ));
+                  if (res == "Oui") {
+                    setState(() {
+                      state.isLoading = true;
+                    });
+                    await Future.delayed(Duration(seconds: 3));
+                    print(
+                        "WiDGETINTERN ${widget.phaseIntervenantId}, phaseId ${widget.phaseId}");
+                    var ctrl = ref.read(voteCtrlProvider.notifier);
+                    await ctrl.sendVoteResulats(
+                        widget.phaseIntervenantId, widget.phaseId);
+                    if (mounted) {
+                      setState(() {
+                        state.isLoading = false;
+                      });
+                      await _chargement(context, ref);
+                      context.pop(true);
+                    }
+                  }
+                }, child: Text("Valider",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xffFF7900).withOpacity(0.8),
+                    )
+                )
+        ),
+      ),
+    );
   }
 }
